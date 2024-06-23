78791b2cd795d576655b7a99e630a60d4a46eec0
tabishkhan03 <tabish25u@gmail.com>
2024-06-23 21:43:18 +0530

    change the path in sendmail

--- src/app/api/sendmail/route.ts
+++ src/app/api/sendmail/route.ts
@@ -7,8 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   try {
-    const file = await fs.readFile(
-      process.cwd() + "/src/app/emailcount.json",
+    const file = await fs.readFile("/src/app/emailcount",
       "utf8"
     );
     const emailcount = JSON.parse(file);
